091e977c60473b6949b15715b47ec61fbd4515bb
nofar2021 <92523988+nofar2021@users.noreply.github.com>
2022-03-06 00:32:08 +0200

    i insert values to db from new petition page

---
+++ new_petition.php
@@ -0,0 +1,44 @@
+<?php
+ include 'db_conn.php';
+ include 'user_details.php';
+ 
+
+ 
+ if(isset($_POST['submit'])){
+
+    $check=getimagesize($_FILES["img"]["tmp_name"]);
+   // $extensions_arr = array("jpg","jpeg","png","gif");
+
+    // Check extension
+	//if( in_array($imageFileType,$extensions_arr) ){
+        if(check!==false){
+
+        $title =  $_REQUEST['subject'];
+        $photo = $_FILES["img"]["name"];
+        $content =  $_REQUEST['Content'];
+        $target = $_REQUEST['sign_num'];
+       // $email = $_REQUEST['uname']; 
+        $date = date("Y/m/d");
+        $alert = $_REQUEST['alert_sign'];
+        
+
+        $sql= "INSERT INTO `all_petitions` VALUES ( NULL, '$title', '$content', '$target', 'nofarefa@gmail.com', '$date', '$photo', '$alert')";
+
+        if(mysqli_query($conn, $sql)){
+            echo '<script> alert ("your petition has creates successfully.") </script>'; 
+            echo "(<script> window.location='new_petition.html';</script>)";
+            //header("Location: new_petition.html");
+  
+        } else{
+            echo " Sorry please try again later $sql. " 
+                . mysqli_error($conn);
+        }
+    }
+    else{
+        echo '<script> alert ("you can insert only jpg, jpeg ,png, gif files") </script>';
+    }
+
+    }
+
+    
+  ?>
