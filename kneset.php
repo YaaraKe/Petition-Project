4948252395e7efa6a59b3979afbef5872b1c22b5
nofar2021 <92523988+nofar2021@users.noreply.github.com>
2022-03-15 23:31:37 +0200

    validation to knesset

--- kneset.php
+++ kneset.php
@@ -42,9 +42,11 @@ $mysqli->close();
         })();
     </script>
     <script>
+       var  flag=0;
         function email_choesen(clicked_id){
             alert(clicked_id);
             document.getElementById("toemail").value=clicked_id;
+            flag=1;
         }
     </script>
 
@@ -91,6 +93,7 @@ $mysqli->close();
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
         crossorigin="anonymous"></script>
+        <link href="petitions_errors.css" rel="stylesheet">
 
     <!-- nav bar for the website -->
     <br>
@@ -116,25 +119,60 @@ $mysqli->close();
                 <li class="nav-item active">
                     <a class="nav-link" href="../kneset.php">Contact Knesset Member<span class="sr-only"></span></a>
                 </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="../my_petition.php">My petitions<span class="sr-only"></span></a>
+                </li>
             </ul>
         </div>
     </nav>
     <br>
     <h1>Send Email to a Knesset Member</h1>
     <br>
-    <div class="col-7">
-        <p>To: <input type="text" id="toname" placeholder="Recipient name"> </p>
-        <p>Your name: <input type="text" id="fromname" placeholder="Your name"> </p>
+    <form class="col-7">
 
-
-        <lable>Message:</lable> <br><textarea id="msg" placeholder="Your message" rows="10" cols="50"></textarea>
-
-
-        <p>Recipient Email address: <input type="text" id="toemail" placeholder="example@gmail.com"></p>
-
-
-        <button onclick='sendMail()'>Send Email</button>
+         <div class="form-control ">
+        <lable>To:</lable> 
+        <input name="to" type="text" id="toname" placeholder="Recipient name" required  >
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen-fill" viewBox="0 0 16 16">
+  <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z"/>
+</svg>
+        <small> Error message </small>
     </div>
+    <div class="form-control">
+        <label>name:</label>  <input type="text" id="fromname" placeholder="Your name" required name="userName"> 
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+</svg>
+        <small> Error message </small>
+    </div>
+    <div class="form-control ">
+        <lable>Message:</lable> <br>
+        <textarea name="msg" id="msg" placeholder="Your message" rows="10" cols="50" required ></textarea>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-quote-fill" viewBox="0 0 16 16">
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793V2zm7.194 2.766a1.688 1.688 0 0 0-.227-.272 1.467 1.467 0 0 0-.469-.324l-.008-.004A1.785 1.785 0 0 0 5.734 4C4.776 4 4 4.746 4 5.667c0 .92.776 1.666 1.734 1.666.343 0 .662-.095.931-.26-.137.389-.39.804-.81 1.22a.405.405 0 0 0 .011.59c.173.16.447.155.614-.01 1.334-1.329 1.37-2.758.941-3.706a2.461 2.461 0 0 0-.227-.4zM11 7.073c-.136.389-.39.804-.81 1.22a.405.405 0 0 0 .012.59c.172.16.446.155.613-.01 1.334-1.329 1.37-2.758.942-3.706a2.466 2.466 0 0 0-.228-.4 1.686 1.686 0 0 0-.227-.273 1.466 1.466 0 0 0-.469-.324l-.008-.004A1.785 1.785 0 0 0 10.07 4c-.957 0-1.734.746-1.734 1.667 0 .92.777 1.666 1.734 1.666.343 0 .662-.095.931-.26z"/>
+</svg>
+        <small> Error message </small>
+</div>
+<div class="form-control ">
+<p style="color:blue;"> Click on a Member of Knesset in the table below</p>
+<label> Recipient Email address:</label>  <input type="text" name="tomail" id="toemail" required disabled>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-check-fill" viewBox="0 0 16 16">
+  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 4.697v4.974A4.491 4.491 0 0 0 12.5 8a4.49 4.49 0 0 0-1.965.45l-.338-.207L16 4.697Z"/>
+  <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-1.993-1.679a.5.5 0 0 0-.686.172l-1.17 1.95-.547-.547a.5.5 0 0 0-.708.708l.774.773a.75.75 0 0 0 1.174-.144l1.335-2.226a.5.5 0 0 0-.172-.686Z"/>
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-index-thumb-fill" viewBox="0 0 16 16">
+  <path d="M8.5 1.75v2.716l.047-.002c.312-.012.742-.016 1.051.046.28.056.543.18.738.288.273.152.456.385.56.642l.132-.012c.312-.024.794-.038 1.158.108.37.148.689.487.88.716.075.09.141.175.195.248h.582a2 2 0 0 1 1.99 2.199l-.272 2.715a3.5 3.5 0 0 1-.444 1.389l-1.395 2.441A1.5 1.5 0 0 1 12.42 16H6.118a1.5 1.5 0 0 1-1.342-.83l-1.215-2.43L1.07 8.589a1.517 1.517 0 0 1 2.373-1.852L5 8.293V1.75a1.75 1.75 0 0 1 3.5 0z"/>
+</svg>
+        <small> Error message </small>
+        </div> 
+   
+        <button onclick=" return kneset_validate()" class="btn btn-outline-primary">Send Email</button> 
+        <button type="reset" value="Reset" name="reset" class="btn btn-outline-secondary">Reset</button>
+
+    </form>
+
+
+    
 
     <section>
         <h1>Contacts Member</h1>
@@ -168,6 +206,77 @@ $mysqli->close();
     </section>
     <br>
 
+    <!-- validation form -->
+    <script>
+        
+        function kneset_validate(){
+    
+   var  to_send= document.getElementById("toname");
+     var msg=document.getElementById("msg");
+     var tomail=document.getElementById("toemail");
+     var userName=document.getElementById("fromname");
+     var counter=0;
+     
+    
+
+    if(to_send.value.match(/\D/) == null){
+        
+        setErrorFor(to_send,"\n" +"You must enter some content.");
+        
+    }
+    else{
+     setSuccessFor(to_send);
+     counter++;
+    }
+ if(msg.value.match(/\D/) === null){
+    
+         setErrorFor(msg,"\n" +"You must enter some content to your message.");
+     }
+     else if(msg.value.length<5) {
+         setErrorFor(msg,"\n" +"Message content must be longer than 5 characters.");
+     }
+     else{
+ setSuccessFor(msg);
+ counter++;}
+
+ if(userName.value.match(/\D/) === null ){
+   
+         setErrorFor(userName,"\n" +"You must enter some content to the name.");
+     }
+     else{
+ setSuccessFor(userName);
+ counter++;
+ }
+
+ if(flag==0){
+    setErrorFor(tomail,"\n" +"You must select an Email address from the table below.");
+ }
+ else{
+    setSuccessFor(tomail);
+ counter++;
+ }
+if(counter==4){
+ sendMail();
+ alert("Your message was sent successfully.")
+ return true;
+}
+else{
+    return false;
+}
+}
+        function setErrorFor(input, msg){
+           
+    const formControl=input.parentElement;
+          const small=formControl.querySelector("small");
+          small.innerText=msg;
+          formControl.className='form-control error';
+  }
+  
+  function setSuccessFor(input){
+      const formControl=input.parentElement;
+      formControl.className='form-control success';
+  }
+        </script>
 
 </body>
 
--- validations.js
+++ validations.js
@@ -11,48 +11,54 @@ function ValidatePetitionForm(){
     var counter=0;
     
 if(subject.value.match(/\D/) == null){
-    setErrorFor(subject,"\n" +"You must enter letters to your subject.");
+    setErrorFor(subject,"\n" +"You must enter some content.");
+    
 }
    else if(subject.value.length>50 || subject.value.length<5){
         setErrorFor(subject, "Length: "+ ssubject.value.length + "\n" +"Title length must be between 5 and 50 characters."  );
-        return false;
+       
     } 
     else{
         setSuccessFor(subject);
         counter++;
      }
      if(content.value.match(/\D/) == null){
-        setErrorFor(content,"\n" +"You must enter letters to your content.");
-        return false;
+        setErrorFor(content,"\n" +"You must enter some content.");
+       
      }
      else{
         
          if(content.value.length>65535 || content.value.length<10){
             setErrorFor(content,"Length: "+ content.value.length +  "\n" +"Your content must be between 10 to 65,535 characters.");
-            return false;
+            
          }
          else{
             setSuccessFor(content);
             counter++;
          }
      }
-     if(target.value<1){
-        setErrorFor(target,"\n" +"Target signatures must be a positive number.");
-        return false;
+     if(target.value===null){
+        setErrorFor(target,"\n" +"You must pick a number.");
      }
-     else if (target.value>999999999){
+     else if(targetv_INT.value<1){
+        setErrorFor(target,"\n" +"Target signatures must be a positive number.");
+        
+     }
+     else if (targetv_INT.value>999999999){
         setErrorFor(target,"\n" +"Please try to select a lower target signatures.");
-        return false;
+       
      }
      else{
         setSuccessFor(target);
         counter++;
      }
+     if(alertn.value===null){
+        setErrorFor(alertn,"\n" +"You must pick a number.");
+     }
 
-     if(alertf_INT>=targetv_INT){
-         alert( "yay")
+     else if(alertf_INT>=targetv_INT){
         setErrorFor(alertn,"\n" +"The number of signatures to be notified must be lower than the target signature. We will also notify you when you reach your full destination:" + "\n" +target.value +".");
-        return false;
+      
      } 
      else{
         setSuccessFor(alertn);
@@ -61,10 +67,13 @@ if(subject.value.match(/\D/) == null){
    if(counter==4){
        return true;
    }
+   else{ return false;}
 
 
 
 }
+
+
 
 function sign_name(){
     var namef = document.signedForm.signName;
