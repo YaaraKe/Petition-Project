263849f2b0b2835b0de59bb49475b2c04c63f42e
anafri96 <chana-aviva.friberg@hotmail.com>
2022-03-23 17:58:53 +0200

    links

--- ana13_3/src/Shop/Shop.js
+++ ana13_3/src/Shop/Shop.js
@@ -1,73 +1,71 @@
-import React from 'react';
-import Button from './Button';
-import paper from '../picShop/paper.jpg';
-import scotch from '../picShop/scotch.jpg';
-import sharpi from '../picShop/Sharpi.jpg';
-import colorsharp from '../picShop/Sharpi1.jpg';
-import './shop.css'
+import React from "react";
+import Button from "./Button";
+import paper from "../picShop/paper.jpg";
+import scotch from "../picShop/scotch.jpg";
+import sharpi from "../picShop/Sharpi.jpg";
+import colorsharp from "../picShop/Sharpi1.jpg";
+import "./shop.css";
 //bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 //Axios for get request
-import axios from 'axios'
+import axios from "axios";
 
-    //render() method, this method returns HTML.
-const Shop = ()=> {
-  // constructor(props) {
-  //   super(props)
-  //     this.state = {
-  //       data: []
-  //             }
-  //     }
+//render() method, this method returns HTML.
+const Shop = () => {
+  const art = {
+    amount1: 94.12,
+    amount2: 37,
+    amount3: 35.08,
+    amount4: 25.6,
+    title1: "Paper 01",
+    title2: "Scotch 02",
+    title3: "Sharpie v2",
+    title4: "Sharpie v1",
+  };
 
-  //     axios.get('http://localhost/save.php').then(res => 
-  //       {
-        
-  //       this.setState({data: res.data});
-  //          }); 
-        
-  //       }
-      const art = 
-        {
-          amount1: 94.12,
-          amount2: 37,
-          amount3: 35.08,
-          amount4: 25.60,
-          title1 : "Paper 01",
-          title2 : "Scotch 02",
-          title3 : "Sharpie v2",
-          title4 : "Sharpie v1",
-          link1: "lol",
-  
-        }
+  // const art1 = [
+  //   {
+  //     amount: 94.12,
+  //     title: "Paper 01",
+  //     link: "heyhey",
+  //   }
+  // ];
 
-
-      return (
-        <>
-          <div class="border">
-            <h1>{art.title1}</h1>
-            <img src={paper} alt=""></img>
-            <div>$ {art.amount1}</div>
-            <Button link={art.link1}></Button>
-          </div>
-          <div class="border">
-            <h1>{art.title2}</h1>
-            <img src={scotch} alt=""></img>
-            <div>$ {art.amount2}</div>
-            <Button link="..\Shop\pageShop\picProduct2\product2.html"></Button>
-          </div>
-          <div class="border">
-            <h1>{art.title3}</h1>
-            <img src={sharpi} alt=""></img>
-            <div>$ {art.amount3}</div>
-            <Button link="..\Shop\pageShop\picProduct3\product3.html"></Button>
-          </div>
-          <div class="border">
-            <h1>{art.title4}</h1>
-            <img src={colorsharp} alt=""></img>
-            <div>$ {art.amount4}</div>
-            <Button link="..\Shop\pageShop\picProduct4\product4.html"></Button>
-          </div>
-        </>
-      );
-    }
-export default Shop;  
+  return (
+    <>
+      {/* {art.map((artItem) => (
+        <div class="border">
+          <h1>{artItem.title}</h1>
+          <img src={paper} alt=""></img>
+          <div>$ {artItem.amount}</div>
+          <Button link={artItem.link} />
+        </div>
+      ))} */}
+      <div class="border">
+        <h1>{art.title1}</h1>
+        <img src={paper} alt=""></img>
+        <div>$ {art.amount1}</div>
+        <Button link={"artItem.link"} />
+      </div>
+       <div class="border">
+        <h1>{art.title2}</h1>
+        <img src={scotch} alt=""></img>
+        <div>$ {art.amount2}</div>
+        <Button link={"..ShoppageShoppicProduct2product2.html"} />
+      </div>
+      <div class="border">
+        <h1>{art.title3}</h1>
+        <img src={sharpi} alt=""></img>
+        <div>$ {art.amount3}</div>
+        <Button link={"..ShoppageShoppicProduct3product3.html"} />
+      </div>
+      <div class="border">
+        <h1>{art.title4}</h1>
+        <img src={colorsharp} alt=""></img>
+        <div>$ {art.amount4}</div>
+        <Button link={"..ShoppageShoppicProduct4product4.html"} />
+      </div>
+    </>
+  );
+};
+export default Shop;
