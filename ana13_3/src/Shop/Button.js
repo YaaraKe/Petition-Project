36372828cfc7eec4d3c89d35141f155d2e97de70
anafri96 <chana-aviva.friberg@hotmail.com>
2022-03-28 11:09:38 +0300

    react a href change

--- ana13_3/src/Shop/Button.js
+++ ana13_3/src/Shop/Button.js
@@ -3,10 +3,10 @@ import "./button.css";
 
 const Button = ({ link }) => {
 
+  //todo : check if a need to be before the button
   return (
-    <button id="button" onClick={() => console.log(link)}>
-      <a id="links" href={link}>Buy</a>
-    </button>
+    <a id="links" href={link}> <button id="button" onClick={() => console.log(link)}>
+      Buy</button></a>
   );
 };
 export default Button;
