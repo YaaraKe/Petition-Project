42e901409ec84efaa0ab316bf508ebc3820bde67
anafri96 <chana-aviva.friberg@hotmail.com>
2022-04-03 00:07:27 +0300

    chnage of h1 to h3 in shop.js

--- ana13_3/src/Shop/Shop.js
+++ ana13_3/src/Shop/Shop.js
@@ -13,14 +13,14 @@ import axios from "axios";
 //render() method, this method returns HTML.
 const Shop = () => {
   const art = {
-    amount1: 94.12,
-    amount2: 37,
-    amount3: 35.08,
-    amount4: 25.6,
-    title1: "Paper 01",
-    title2: "Scotch 02",
-    title3: "Sharpie v2",
-    title4: "Sharpie v1",
+    amount1: 20,
+    amount2: 5,
+    amount3: 2,
+    amount4: 6,
+    title1: "Pacon Peacock Poster",
+    title2: "Scotch Sure Packagin",
+    title3: "Sharpie Fine Tip Mar",
+    title4: "Sharpie Pocket Highl",
   };
 
   // const art1 = [
@@ -42,25 +42,25 @@ const Shop = () => {
         </div>
       ))} */}
       <div class="border">
-        <h1>{art.title1}</h1>
+        <h3>{art.title1}</h3>
         <img src={paper} alt=""></img>
         <div>$ {art.amount1}</div>
         <Button link={"../pageShop/picProduct1/product1.php"} />
       </div>
        <div class="border">
-        <h1>{art.title2}</h1>
+        <h3>{art.title2}</h3>
         <img src={scotch} alt=""></img>
         <div>$ {art.amount2}</div>
         <Button link={"../pageShop/picProduct2/product2.php"} />
       </div>
       <div class="border">
-        <h1>{art.title3}</h1>
+        <h3>{art.title3}</h3>
         <img src={sharpi} alt=""></img>
         <div>$ {art.amount3}</div>
         <Button link={"../pageShop/picProduct3/product3.php"} />
       </div>
       <div class="border">
-        <h1>{art.title4}</h1>
+        <h3>{art.title4}</h3>
         <img src={colorsharp} alt=""></img>
         <div>$ {art.amount4}</div>
         <Button link={"../pageShop/picProduct4/product4.php"} />
