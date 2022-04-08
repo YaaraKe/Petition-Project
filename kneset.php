a516fc4fc9b257b32129378d7e39d9524e680785
YaaraKe <yaarakeller@gmail.com>
2022-04-08 18:05:12 +0300

    selectall

--- kneset.php
+++ kneset.php
@@ -53,6 +53,20 @@ $mysqli->close();
             
         }
     </script>
+    <script>
+        function choose_all() {
+            <?php
+            include 'db_conn.php';
+            $sql1="SELECT * FROM Knesset_Member ORDER BY full_name ASC ";
+            $result=mysqli_query($conn, $sql1);
+            while ($row=mysqli_fetch_assoc($result)){
+                ?>
+                email_choesen('<?php echo $row["Email"]; ?>')
+                <?php
+            }
+            ?>
+        }
+    </script>
     <link rel="stylesheet" href="kneset.css">
 </head>
 
@@ -135,7 +149,14 @@ $mysqli->close();
                     <small> Error message </small>
                 </div>
                 <div class="form-control ">
-                    <p style="color:blue;"> Click on a Member of Knesset in the list below</p>
+                    <p style="color:blue;"> Click on a Member of Knesset in the list below or select all</p>
+                    <button onclick="choose_all()" class="btn btn-primary btn-sm"> <svg
+                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992a.252.252 0 0 1 .02-.022zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486-.943 1.179z" />
+                    </svg> | Select All</button>
+                    <br>
                     <label> Recipient Email address:</label> <input type="text" name="tomail" id="toemail" required
                         disabled>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
