f968e3f6093d5a3db49c9b33fbf6ebc7982f648b
nofar2021 <92523988+nofar2021@users.noreply.github.com>
2022-04-27 16:14:29 +0300

    validate image in new petition page

--- validations.js
+++ validations.js
@@ -1,6 +1,7 @@
 function ValidatePetitionForm(){
     
     var subject = document.petitionForm.subject;
+    var img = document.petitionForm.img;
     var content = document.petitionForm.Content;
     var target = document.petitionForm.sign_num;
     var alertf = document.petitionForm.alert_sign.value;
@@ -71,7 +72,16 @@ else if(blacklist.test(subject.value)){
         setSuccessFor(alertn);
         counter++;
      }
-   if(counter==4){
+     if(img.value===""){
+         setErrorFor(img,"\n" +"You must upload a image.");
+         
+     }
+     else{
+          setSuccessFor(img);
+        counter++;
+     
+     }
+   if(counter==5){
        return true;
    }
    else{ return false;}
@@ -80,6 +90,15 @@ else if(blacklist.test(subject.value)){
 
 }
 
+// function serachbox(){
+//     var search = document.search.char;
+//   var blacklist=/["';`]+/;
+//   if(blacklist.test(search.value)){
+//         setErrorFor(search,"\n" +"You can not enter spacial characters.");
+//         return false;
+//   }
+//   return true;
+// }
 
 
 function sign_name(){
