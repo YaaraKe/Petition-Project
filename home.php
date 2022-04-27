218fab58ae5e31fed842dbabac8fd94591aef55f
YaaraKe <yaarakeller@gmail.com>
2022-04-27 14:15:15 +0300

    nav

--- home.php
+++ home.php
@@ -79,7 +79,7 @@ if (isset($_SESSION['user_name'])) {
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">My Account<span
                                 class="sr-only"></span></a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Profile</a>
+                            <a class="dropdown-item" href="p_form.php">Profile</a>
                             <a class="dropdown-item" href="../my_petition.php">My Petitions</a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="logout.php">Log Out</a>
--- send_or_delete_petition.php
+++ send_or_delete_petition.php
@@ -68,7 +68,7 @@
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">My Account<span
                         class="sr-only"></span></a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Profile</a>
+                    <a class="dropdown-item" href="p_form.php">Profile</a>
                     <a class="dropdown-item" href="../my_petition.php">My Petitions</a>
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="logout.php">Log Out</a>
