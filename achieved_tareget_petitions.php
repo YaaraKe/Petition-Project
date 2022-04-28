6410df2445c002f02ddb2b65e44e973cf836fdde
anafri96 <chana-aviva.friberg@hotmail.com>
2022-04-28 11:55:36 +0300

    new desgin for chieved_target

--- achieved_tareget_petitions.php
+++ achieved_tareget_petitions.php
@@ -68,10 +68,9 @@
 
     </nav>
 
-    <br>
-    <hr>
     <div class="container" style="position: relative;text-align: left;">
         <div class="row">
+            <div class="col-2"></div>
             <div class="col-3 mx-auto img_jump" style="float: right;">
                 <img src="../photos/juming.gif" alt="welcome_img" style="width:100%">
             </div>
@@ -80,8 +79,7 @@
                 <br>
                 <br>
                 <br>
-                <br>
-                <p id="text_con">We are happy to present you all the Petitions that have been <span id="bold_p">successfully completed</span>, thanks to all the people that signed and made a difference</p>
+                <p id="text_con" class="show">We are happy to present you all the Petitions that have been <span id="bold_p">successfully completed</span>, thanks to all the people that signed and made a difference</p>
                 <br>
             </div>
         </div>
