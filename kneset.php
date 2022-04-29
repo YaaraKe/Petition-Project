53d456ad0a24c37c3f428a47884103fb22562808
anafri96 <chana-aviva.friberg@hotmail.com>
2022-04-29 11:59:30 +0300

    added information button to kneste

--- kneset.php
+++ kneset.php
@@ -16,6 +16,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="script.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
     <script type="text/javascript">
         (function() {
@@ -131,8 +132,19 @@
                 <br>
                 <br>
                 <h3> Choose to whom the Email will be sent </h3>
+                <details id="information">
+                    <summary><i style="font-size:18px" class="fa">&#xf05a;</i></summary>
+                    <p id="info"><u>Steps to send a email:</u><br>
+                        1. Please press the button of whom you like to send the email.<br>
+                        2. Scoll down until you reach the title "Send Email to a Knesset Member".<br>
+                        3. Write all the details asked in the form.<br>
+                        4. After finishing please press "send email"<br><b>
+                            Thank you for choosing UcanClaim
+                        </b></p>
+                </details>
             </form>
             <?php
+
 
             if (isset($_POST["submit2"])) {
                 include_once("db_conn.php");
