ec4dc646aab2f6a6f60ecd0f2c811dc7ee7e4cbc
anafri96 <chana-aviva.friberg@hotmail.com>
2022-05-01 21:12:20 +0300

    add new products

--- ana13_3/src/Shop/Shop.js
+++ ana13_3/src/Shop/Shop.js
@@ -5,6 +5,9 @@ import scotch from "../picShop/scotch.jpg";
 import sharpi from "../picShop/Sharpi.jpg";
 import poster from "../picShop/shop_gif2.gif";
 import colorsharp from "../picShop/Sharpi1.jpg";
+import mic from "../picShop/mic.jpg";
+import stapler from "../picShop/stapler.jpg";
+import cliper from "../picShop/cliper.jpg";
 import "./shop.css";
 //bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -14,21 +17,26 @@ import axios from "axios";
 //render() method, this method returns HTML.
 const Shop = () => {
   const art = {
-    amount1: "5.00",
-    amount2: "15.00",
-    amount3: "6.00",
-    amount4: "12.00",
-    title1: "Poster Board",
-    title2: "Scotch",
-    title3: "Sharpie Fine Tip Marker",
-    title4: "Sharpie Accent Pocket",
+    title1: "	Poster Board	",
+    title2: "	Scotch	",
+    title3: "	Sharpie Fine Tip Marker	",
+    title4: "	Sharpie Accent Pocket	",
+    title5: "	Blue Microphone	",
+    title6: "	Heavy-Duty Binder Clips	",
+    title7: "	Desktop Stapler	",
+    amount1: "	5	",
+    amount2: "	15	",
+    amount3: "	6	",
+    amount4: "	12	",
+    amount5: "	120	",
+    amount6: "	20	",
+    amount7: "	24	",
   };
 
   return (
     <>
       <img id="poster" src={poster} alt=""></img>
       <h3>Shop</h3>
-
       <div class="border_1">
         <img class="img_shop" src={paper} alt=""></img>
         <b>{art.title1}</b>
@@ -52,6 +60,24 @@ const Shop = () => {
         <b>{art.title4}</b>
         <div>{art.amount4} ₪</div>
         <Button link={"../pageShop/product.php?data=40001"} />
+      </div>
+      <div class="border_1">
+        <img class="img_shop" src={mic} alt=""></img>
+        <b>{art.title5}</b>
+        <div>{art.amount5} ₪</div>
+        <Button link={"../pageShop/product.php?data=50001"} />
+      </div>{" "}
+      <div class="border_1">
+        <img class="img_shop" src={cliper} alt=""></img>
+        <b>{art.title6}</b>
+        <div>{art.amount6} ₪</div>
+        <Button link={"../pageShop/product.php?data=60001"} />
+      </div>{" "}
+      <div class="border_1">
+        <img class="img_shop" src={stapler} alt=""></img>
+        <b>{art.title7}</b>
+        <div>{art.amount7} ₪</div>
+        <Button link={"../pageShop/product.php?data=70001"} />
       </div>
       <div id="clear"></div>
       <div class="bg-white">
