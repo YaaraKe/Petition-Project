7acd2116525600305c2af1ed5b47ed2088dcc282
YaaraKe <yaarakeller@gmail.com>
2022-05-03 10:57:31 +0300

    navbar

--- signed_petition.php
+++ signed_petition.php
@@ -72,7 +72,8 @@
                         class="sr-only"></span></a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="p_form.php">Profile</a>
-                    <a class="dropdown-item" href="../my_petition.php">My Petitions</a>
+                    <a class="dropdown-item" href="../my_petition.php">Created Petitions</a>
+                            <a class="dropdown-item" href="../my_signed_petitions.php">Signed Petitions</a>
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="logout.php">Log Out</a>
                 </div>
