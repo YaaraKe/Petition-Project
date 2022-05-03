a414b2a8774510c1cae5dc012e264a515a32d77c
YaaraKe <yaarakeller@gmail.com>
2022-05-03 10:24:54 +0300

    new

---
+++ my_signed_petitions.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My petitions</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+        <!-- IMPORT BOOTSTRAP SCRIPTS-->
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+
+    <!-- nav bar for the website -->
+    <br>
+    <nav class="navbar navbar-expand-md navbar-light" style="background-color :#F0B27A">
+
+        <a class="navbar-brand" href="#">
+            <img src="../NavBar/UcanClaim.png" width="85" height="40" class="d-inline-block align-top" alt="">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="container">
+            <div class="collapse navbar-collapse justify-content-between " id="navbarNav">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../home.php">Home<span class="sr-only"></span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Petition<span
+                                class="sr-only"></span></a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="all_petitions.php">Sign a Petition</a>
+                            <a class="dropdown-item" href="new_petition.html">Create a Petition</a>
+                            <a class="dropdown-item" href="achieved_tareget_petitions.php">Completed petitions</a>
+                        </div>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../index_react/index.html">Shop<span class="sr-only"></span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../kneset.php">Contact a Knesset Member<span
+                                class="sr-only"></span></a>
+                    </li>
+
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">My Account<span
+                                class="sr-only"></span></a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="p_form.php">Profile</a>
+                            <a class="dropdown-item" href="../my_petition.php">Petitions created by me</a>
+                            <a class="dropdown-item" href="../my_signed_petitions.php">Petitions signed by me</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="logout.php">Log Out</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </nav>
+
+    <br>
+<!-- display all the petition that user created-->
+    <section>
+    <div class="container">
+    <h1> My petitions</h1>
+        <!-- PHP CODE TO FETCH DATA FROM ROWS-->
+        <?php    
+       session_start();
+       include_once("db_conn.php");
+       session_start();
+        $email=$_SESSION['user_name'];
+ 
+ $sql = "SELECT * FROM all_petitions AS p WHERE p.id_petition IN (SELECT id_petition FROM signatures WHERE `email_signed`='" . mysqli_escape_string($conn,$email) . "')";
+       // check the query
+       $result = mysqli_query($conn,$sql); 
+       if ($result === FALSE) {
+        die(mysqli_error($conn));
+      }
+      if(mysqli_num_rows($result)!=0){
+           ?>
+       
+       <div class="row">
+       <?php
+      
+        // LOOP TILL END OF DATA
+                while($record=mysqli_fetch_assoc($result))
+                {
+                    
+             ?>
+
+                
+        <div class="col-md-7">
+            <div class="card">
+                <div class="text-center">
+  
+
+            <?php  echo '<img style="width: 18rem;" src="data:image/jpeg;base64,'.base64_encode( $record['photo'] ).'" class="card-img-top"/>';?>
+            </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-7">
+                            <h5 class="card-title">
+                           
+                        <?php echo $record['title']; ?>
+                   
+                </div> </strong></h5>
+                        </div>
+                        <div class="desc text-muted">
+                    <?php echo $record['date']; ?>
+                </div>
+                        <div class=" ">
+                            <p class="card-text"><?php echo $record['content']; ?></p>
+                        </div>
+                    </div>
+                    <a href="send_or_delete_petition.php?data=<?php echo $record['id_petition']; ?>" class="btn btn-primary">           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-text" viewBox="0 0 16 16">
+  <path d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z"/>
+  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/>
+</svg> | To my petition</a>
+                </div>
+                <br>
+            </div>
+            <br>
+
+        <?php 
+    
+    }
+    
+}
+// In case user did not sign a petition
+else{
+    echo "you still have not signed on petitions, You can sign on ";
+     ?>
+    <a href="all_petitions.php">here.</a>
+    <?php
+}
+  ?>
+
+</body>
+
+</html>
