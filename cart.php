a15358269c27da23e8f67535f7d8f9fa19a01ccc
anafri96 <chana-aviva.friberg@hotmail.com>
2022-05-11 12:46:54 +0300

    oder works withput google

--- cart.php
+++ cart.php
@@ -2,24 +2,42 @@
 
 $conn = mysqli_connect('localhost', 'nofarrei_user', '12345', 'nofarrei_Petition') or die('connection failed');
 
-if (isset($_POST['update_update_btn'])) {
-    $update_value = $_POST['update_quantity'];
-    $update_id = $_POST['update_quantity_id'];
-    $update_quantity_query = mysqli_query($conn, "UPDATE `cart` SET quantity = '$update_value' WHERE id = '$update_id'");
-    if ($update_quantity_query) {
-        header('location:cart.php');
+if (isset($_POST['order_btn'])) {
+
+    $today = date("Ymd");
+    $rand = strtoupper(substr(uniqid(sha1(time())), 0, 4));
+    $unique = $today . $rand;
+
+    $cart_query = mysqli_query($conn, "SELECT * FROM `cart`");
+    $price_total = 0;
+    if (mysqli_num_rows($cart_query) > 0) {
+        while ($product_item = mysqli_fetch_assoc($cart_query)) {
+            $product_name[] = $product_item['name'] . ' (' . $product_item['quantity'] . ') ';
+            $product_price = number_format($product_item['price'] * $product_item['quantity']);
+            $price_total += $product_price;
+        };
     };
-};
 
-if (isset($_GET['remove'])) {
-    $remove_id = $_GET['remove'];
-    mysqli_query($conn, "DELETE FROM `cart` WHERE id = '$remove_id'");
-    header('location:cart.php');
-};
+    $total_product = implode(', ', $product_name);
+    $detail_query = mysqli_query($conn, "INSERT INTO `order`(id, total_products, total_price, date) VALUES('$unique','$total_product','$price_total',now())") or die('query failed');
 
-if (isset($_GET['delete_all'])) {
-    mysqli_query($conn, "DELETE FROM `cart`");
-    header('location:cart.php');
+    if ($cart_query && $detail_query) {
+        echo "
+      <div class='order-message-container'>
+      <div class='message-container'>
+         <h3>thank you for shopping!</h3>
+         <div class='order-detail'>
+          <span>oder number: " . $unique . "</span>
+            <span>" . $total_product . "</span>
+            <span class='total'> total : $" . $price_total . "/-  </span>
+         </div>
+            <a href='../index_react/index.html' class='btn'>continue shopping</a>
+         </div>
+      </div>
+      ";
+
+        mysqli_query($conn, "DELETE FROM `cart`");
+    }
 }
 
 ?>
@@ -31,106 +49,54 @@ if (isset($_GET['delete_all'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cart</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="../pageShop/pageshop.js"></script>
-    <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css' rel='stylesheet'>
-    <script type='text/javascript' src=''></script>
-    <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
-    <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js'></script>
+    <title>checkout</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
     <!-- custom css file link  -->
     <link rel="stylesheet" href="../style.css">
 
 </head>
 
 <body>
-    <!-- IMPORT BOOTSTRAP SCRIPTS-->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <!-- NavBar -->
-    <nav id="navbar"> </nav>
+
 
     <div class="container">
 
-        <section class="shopping-cart">
+        <section class="checkout-form">
 
-            <h1 class="heading">shopping cart</h1>
+            <h1 class="heading">complete your order</h1>
 
-            <table>
+            <form action="" method="post">
 
-                <thead>
-                    <th>image</th>
-                    <th>name</th>
-                    <th>price</th>
-                    <th>quantity</th>
-                    <th>total price</th>
-                    <th>action</th>
-                </thead>
-
-                <tbody>
-
+                <div class="display-order">
                     <?php
-
                     $select_cart = mysqli_query($conn, "SELECT * FROM `cart`");
+                    $total = 0;
                     $grand_total = 0;
                     if (mysqli_num_rows($select_cart) > 0) {
                         while ($fetch_cart = mysqli_fetch_assoc($select_cart)) {
+                            $total_price = number_format($fetch_cart['price'] * $fetch_cart['quantity']);
+                            $grand_total = $total += $total_price;
                     ?>
-
-                            <tr>
-                                <td><img src="uploaded_img/<?php echo $fetch_cart['image']; ?>" height="100" alt=""></td>
-                                <td><?php echo $fetch_cart['name']; ?></td>
-                                <td>$<?php echo number_format($fetch_cart['price']); ?>/-</td>
-                                <td>
-                                    <form action="" method="post">
-                                        <input type="hidden" name="update_quantity_id" value="<?php echo $fetch_cart['id']; ?>">
-                                        <input type="number" name="update_quantity" min="1" value="<?php echo $fetch_cart['quantity']; ?>">
-                                        <input type="submit" value="update" name="update_update_btn">
-                                    </form>
-                                </td>
-                                <td>$<?php echo $sub_total = number_format($fetch_cart['price'] * $fetch_cart['quantity']); ?>/-</td>
-                                <td><a href="cart.php?remove=<?php echo $fetch_cart['id']; ?>" onclick="return confirm('remove item from cart?')" class="delete-btn"> <i class="fas fa-trash"></i> remove</a></td>
-                            </tr>
+                            <span><?= $fetch_cart['name']; ?>(<?= $fetch_cart['quantity']; ?>)</span>
                     <?php
-                            $grand_total += $sub_total;
-                        };
-                    };
+                        }
+                    } else {
+                        echo "<div class='display-order'><span>your cart is empty!</span></div>";
+                    }
                     ?>
-                    <tr class="table-bottom">
-                        <td><a href="../index_react/index.html" class="option-btn" style="margin-top: 0;">continue shopping</a></td>
-                        <td colspan="3">grand total</td>
-                        <td id="price1">$<?php echo $grand_total; ?>/-</td>
-                        <td><a href="cart.php?delete_all" onclick="return confirm('are you sure you want to delete all?');" class="delete-btn"> <i class="fas fa-trash"></i> delete all </a></td>
-                    </tr>
-
-                </tbody>
-
-            </table>
-
-            <div class="checkout-btn">
-                <a href="checkout.php" class="btn <?= ($grand_total > 1) ? '' : 'disabled'; ?>">procced to checkout</a>
-            </div>
+                    <span class="grand-total"> grand total : $<?= $grand_total; ?>/- </span>
+                </div>
+                <div class="flex">
+                    <input type="submit" value="order now" name="order_btn" class="btn">
+            </form>
 
         </section>
-        <div id="container"></div>
-        <div id="update" style="display:none"></div>
-        <script async src="https://pay.google.com/gp/p/js/pay.js" onload="onGooglePayLoaded()"></script>
-        <!--document.getElementById('price1').innerHTML-->
-
 
     </div>
-    <!-- footer -->
-    <div id="footer"></div>
+
 </body>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script>
-    $("document").ready(function() {
-        //  navbar
-        $("#navbar").load("../common/NavBar.php");
-        //  footer
-        $("#footer").load("../common/footer.html");
-    });
-</script>
 
 </html>
